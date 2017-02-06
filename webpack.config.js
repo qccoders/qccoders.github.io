0242dcd271ce2aefba066078b3905224df697287
Michael Liendo <model3volution@gmail.com>
2017-02-06 14:46:57 -0600

    add language

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
 //require in the built-in node-module used for paths (directories)
 const path = require('path')
 
+
 //when someone requires this file (webpack.config.js), what they'll get back is whatever is exported
+
+/*
+1. Export this object using module.exports
+2. Bundle everything from index.js
+3. Take the bundled file and put it in the public/dist folder (creates one if not make)
+4. Name that bundled file bundle.js
+5. Peform some actions on the bundled code.
+  -Load run it through babel, which in .babelrc I use 'env' and 'react'
+    -meaning, convert my react code to regular javascript
+    - convert my es2015+ code to JS that can be ran on any browser
+  -Perform that on any file ending in .js
+  -Exclude the node_modules folder
+*/
 module.exports = {
   entry: "./public/index.js",
   output: {
